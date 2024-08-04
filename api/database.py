da5e55bdacbcb9d8ed0c5f38815d914af41e460a
fumifumitaro <51777014+fumifumitaro@users.noreply.github.com>
2024-08-04 16:18:00 +0900

    マイグレーション環境の用意

---
+++ api/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
+Session = sessionmaker(bind=engine)
