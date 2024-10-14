678cec2fd2bc28f58fe82c1b804b74699bc99bc4
noa2022 <101255958+noa2022@users.noreply.github.com>
2024-10-14 19:23:54 +0900

    メッセージ一覧取得/編集/削除API追加

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+from datetime import datetime
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 
@@ -12,6 +13,13 @@ class CreateRequest(BaseModel):
     message: str
     password: str
 
+
+class UpdateRequest(BaseModel):
+    name: str
+    message: str
+    password: str
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -23,6 +31,15 @@ def get_db():
 def read_root():
     return {"message": "Hello, World!"}
 
+
+@app.get("/messages/")
+def read_root(db: Session = Depends(get_db)):
+    data = db.query(Message).all()
+    if not data:
+        raise HTTPException(status_code=404, detail="Message not found")
+    return data
+
+
 @app.post("/messages/")
 def create_message(request: CreateRequest, db: Session = Depends(get_db)):
     data = Message(name=request.name, message=request.message, password=request.password)
@@ -32,3 +49,35 @@ def create_message(request: CreateRequest, db: Session = Depends(get_db)):
     db.refresh(data)
 
     return data.id
+
+
+@app.delete("/messages/{message_id}")
+def delete_message(message_id: int, db: Session = Depends(get_db)):
+    data = db.query(Message).filter(Message.id == message_id).first()
+    
+    if not data:
+        raise HTTPException(status_code=404, detail="Message not found")
+        
+    data.deleted_at = datetime.now()
+    db.commit()
+
+    return {"detail": "Message deleted successfully"}
+
+
+@app.put("/messages/{message_id}")
+def update_message(request: UpdateRequest, message_id: int, db: Session = Depends(get_db)):
+    data = db.query(Message).filter(Message.id == message_id).first()
+
+    if not data:
+        raise HTTPException(status_code=404, detail="Message not found")
+
+    data.name = request.name
+    data.message = request.message
+    data.password = request.password
+
+    db.commit()
+    db.refresh(data)
+
+    return data.id
+
+    
